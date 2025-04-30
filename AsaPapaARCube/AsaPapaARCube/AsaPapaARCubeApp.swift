ce1d12d2d6cb180c2e91829c970142f4ea2ba963
Shinya <emashinya0612@gmail.com>
2025-04-30 15:12:48 +0900

    initial commit for AsaPapaARCube

---
+++ AsaPapaARCube/AsaPapaARCube/AsaPapaARCubeApp.swift
@@ -0,0 +1,18 @@
+//
+//  AsaPapaARCubeApp.swift
+//  AsaPapaARCube
+//  
+//  Created on 2025/04/30
+//
+
+
+import SwiftUI
+
+@main
+struct AsaPapaARCubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
